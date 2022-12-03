76136ad2f0bb35275625c085533e33b4f564c0f2
Almog Ifergan <91954618+Almogif@users.noreply.github.com>
2022-12-03 09:04:31 +0200

    Update memse.c

--- C/memse.c
+++ C/memse.c
@@ -1,11 +1,14 @@
+.C
 
-#include <stdio.h>
+#include <stdio.h> /*size_t*/
+
+#include "ws9.h"
 
 #define WORD_SIZE sizeof(size_t)
 
-void SetChunk(unsigned char *buffer, int c);
-void *MemSet(void *dest, int c, size_t n);
-int IsAligned(void *s);
+static void WriteChunk (void *dest, void *src);
+static void SetChunk(unsigned char *buffer, int c);
+static int IsAligned(void *s);
 
 void *MemSet(void *dest, int c, size_t n)
 {
@@ -92,7 +95,6 @@ void *MemCpy(void *dest, const void *src, size_t n)
     }
 }
 
-
 void *MemMove(void *dest, const void *src, size_t n)
 {
     /*start copy from the end, to avoid overlap*/
@@ -108,22 +110,104 @@ void *MemMove(void *dest, const void *src, size_t n)
     }
 }
 
+test.c 
+/***********************************
+* Date: 2/12/22
+* Authour: Almog 
+* Reviewer: Nadav 
+*************************************/
+#include <stdio.h> /* printf */
+#include <string.h> /*memove,memcpy,memset */
+
+#include "ws9.h"
+
+#define RESET   "\033[0m"           /* Reset color */
+#define RED     "\033[1m\033[31m"   /* Bold Red */
+#define GREEN   "\033[1m\033[32m"   /* Bold Green */
+
+
+void FuncTestChar(unsigned char *out, unsigned char *e_out, const char *name)
+{
+    if (*out != *e_out)
+    {
+        printf("%s "RED" FAILED "RESET" output: %s, expected output: %s  \n",
+        name ,out ,e_out);
+    }
+}
+
+void TestMemFuncs()
+{
+  unsigned char src[33] = "DataDataDataDataDataDataDataData";
+  unsigned char dest[33] = "12341234123412341234123412341234";
+  unsigned char copy_src[33] = "DataDataDataDataDataDataDataData";
+  unsigned char copy_dest[33] = "12341234123412341234123412341234";
+
+    MemMove(dest, src, 10);
+    memmove(copy_dest, copy_src, 10);
+    FuncTestChar(copy_dest, dest, "MemMove");
+
+    MemCpy(dest, src, 10);
+    memcpy(copy_dest, copy_src, 10);
+    FuncTestChar(copy_dest, dest, "MemCpy");
+
+    MemSet(dest, '$', 3);
+    memset(copy_dest, '$', 3);
+    FuncTestChar(copy_dest, dest, "MemSet");
+
+
+}
+
 
 int main()
 {
-  char src[11] = "almogalmog";
-  char dest[18] = "12345671234512345";
-  
-
-    
-MemMove(dest, src, 1);
-printf ("dest: %s\n", dest);
-MemSet(dest, '$', 8);
-printf ("dest: %s\n", dest);
-  printf ("is IsAligned: %d\n", IsAligned(dest));
-
-
-  return 0;
+    TestMemFuncs();
+    return 0;
 }
+
+.h
+#ifndef __WS9_H__
+#define __WS9_H__
+
+#include<stddef.h>
+/******************************************************************************
+* Date: 1/12/22
+* Authour: Almog 
+* Reviewer: Nadav
+*******************************************************************************/
+
+/******************************************************************************
+    memset - fill memory with a constant byte
+    The  memset()  function  fills  the  first  n  bytes of the memory area
+    pointed to by s with the constant byte c.
+    returns a pointer to the memory area s.
+*******************************************************************************/
+void *MemCpy(void *dest, const void *src, size_t n);
+
+
+/******************************************************************************
+    memset - fill memory with a constant byte
+    The  memset()  function  fills  the  first  n  bytes of the memory area
+    pointed to by s with the constant byte c.
+    returns a pointer to the memory area s.
+*******************************************************************************/
+void *MemSet(void *dest, int c, size_t n);
+
+
+/******************************************************************************
+    MemMove - copy memory area
+    copies n bytes from memory area src to memory
+    area dest.  The memory areas may overlap: copying takes place as though
+    the  bytes in src are first copied into a temporary array that does not
+    overlap src or dest, and the bytes are then copied from  the  temporary
+    array to dest.
+    returns a pointer to dest.
+*******************************************************************************/
+void *MemMove(void *dest, const void *src, size_t n);
+
+#endif /* __WS9_H__ */
+
+
+
+
 
 
